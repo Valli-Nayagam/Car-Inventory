630cf4bebd1024457759b12c143d05e5af3e779a
Valli Nayagam <kbsvalli1@gmail.com>
2022-01-22 12:06:57 -0500

    updated

--- server/index.js
+++ server/index.js
@@ -1,10 +1,22 @@
-const express = require('express')
-const app = express()
+const express = require('express');
+const app = express();
+const mysql = require('mysql')
 
-app.get('/', (req, res)=>{
-    res.send('server');
+const db = mysql.createPool({
+    host: "localhost",
+    user: "root",
+    password: "password",
+    database: "carInventory"
+});
+
+app.get('/', (req, res) => {
+
+    const sqlInsert = "INSERT INTO InventoryManagement VALUE('TATA Indica Vista', 'This is one of the brand looking cars of tata withmanual and auto transmission torque convertor', 'Grey', 1800000);"
+    db.query(sqlInsert, (err, results) => {
+        res.send('server');
+    })
 })
 
-app.listen(3001, ()=> {
+app.listen(3001, () => {
     console.log('server');
 })
