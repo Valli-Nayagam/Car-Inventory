dc1aee68ea864730ac8cbe1b3194bdba4ee4c6dc
Valli nayagam <kbsvalli1@gmail.com>
2022-02-19 21:07:52 +0000

    updated

--- server/index.js
+++ server/index.js
@@ -2,12 +2,12 @@ const express = require('express');
 const bodyParser = require('body-parser')
 const cors = require('cors')
 const app = express();
-const mysql = require('mysql')
+const mysql = require('mysql2')
 
 const db = mysql.createPool({
     host: "localhost",
     user: "root",
-    password: "password",
+    password: "root",
     database: "carInventory"
 });
 
